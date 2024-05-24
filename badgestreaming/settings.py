67398f58223d228da3b4bafbf6fe0a1d5e292d57
djokodev <djokodev75@gmail.com>
2024-05-24 11:47:28 +0100

    configuration de la base de donnee postgres dans le projet

--- badgestreaming/settings.py
+++ badgestreaming/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'badgestreaming.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'badgestream',
+        'USER': 'djokobadgestream',
+        'PASSWORD': 'Mesongles3565',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
