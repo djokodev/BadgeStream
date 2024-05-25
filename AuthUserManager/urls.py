af5a402126424ec6261681ae92f4c731cabdcbd9
djokodev <djokodev75@gmail.com>
2024-05-25 19:25:55 +0100

    implementation de la fonctionnalite de changement du mot de passe de l'utilisateur

--- AuthUserManager/urls.py
+++ AuthUserManager/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', ProfilView.as_view(), name='profile'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='AuthUserManager/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='AuthUserManager/password_change_done.html'), name='password_change_done'),
 ]
