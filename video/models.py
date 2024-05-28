17819e5effd64bb708a0cf53aaea654b309d7643
djokodev <djokodev75@gmail.com>
2024-05-28 10:00:47 +0100

    pillow installed

--- video/models.py
+++ video/models.py
@@ -1,2 +1,14 @@
 from django.db import models
+from AuthUserManager.models import CustomeUser
 
+class AnimatedVideo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    file = models.FileField(upload_to='videos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(CustomeUser, on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='thumbnails/', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
