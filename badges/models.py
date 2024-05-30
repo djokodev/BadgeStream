f15463f6df812423b82b82511ce1d88120a77efa
djokodev <djokodev75@gmail.com>
2024-05-30 14:13:53 +0100

    field image removed in model Badge

--- badges/models.py
+++ badges/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Badge(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images_badges/')
     description = models.TextField()
 
     def __str__(self):
--- badges/tests.py
+++ badges/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from .models import Badge
 
-# Create your tests here.
+class TestCaseBadge(TestCase):
+    def setUp(self):
+        pass
